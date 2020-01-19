23b82d4b2d4897ba52343b0c183e6809e26bf9d6
slowhand-lang <59637642+slowhand-lang@users.noreply.github.com>
2020-01-19 14:45:05 -0500

    Create SQL_data_science_quiz_1.sql

---
+++ SQL_data_science_quiz_1.sql
@@ -0,0 +1,17 @@
+--Retrieve all the records from the Employees table.
+
+SELECT * 
+FROM EMPLOYEES
+
+--Retrieve the FirstName, LastName, Birthdate, Address, City, and State from the Employees table.
+
+SELECT FirstName, LastName, Birthdate, Address, City, State
+FROM EMPLOYEES;
+
+--Retrieve all the columns from the Tracks table, but only return 20 rows.
+
+SELECT * 
+FROM TRACKS
+LIMIT 20;
+
+--
